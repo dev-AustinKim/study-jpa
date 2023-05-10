c69c70f51d6266a49d0103f4c00484a2a3bbaa3b
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-10 16:18:23 +0900

    JPA Study(Sprint Security) Day18

--- workspace/security/session/src/main/java/com/app/session/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.session.repository;
-
-import com.app.session.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryDSL {
-}
